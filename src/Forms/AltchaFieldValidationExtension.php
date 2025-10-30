3cd77f5951bd68ef88d90cf1f72838ef2a2df676
Andy Adiwidjaja <adiwidjaja@gmail.com>
2025-10-30 13:57:59 +0100

    Working SS5 + SS6 compatibility.

--- src/Forms/AltchaFieldValidationExtension.php
+++ src/Forms/AltchaFieldValidationExtension.php
@@ -6,11 +6,32 @@ use SilverStripe\Core\Extension;
 
 class AltchaFieldValidationExtension extends Extension
 {
+    /**
+     * Validation for SS5
+     *
+     * @param mixed &$result
+     * @param mixed &$validator
+     * @return void
+     */
     public function updateValidationResult(&$result, &$validator)
     {
         $value = $this->owner->value;
         if(!$value || $this->owner->altcha->verifySolution($value, true) === false) {
             $validator->validationError($this->owner->getName(), 'Altcha Captcha validation failed', 'error');
+        }
+    }
+
+    /**
+     * Validation for SS6
+     *
+     * @param mixed &$result
+     * @return void
+     */
+    public function updateValidate(&$result)
+    {
+        $value = $this->owner->value;
+        if(!$value || $this->owner->altcha->verifySolution($value, true) === false) {
+            $result->addError('Altcha Captcha validation failed', 'error', $this->owner->getName());
         }
     }
 }
